4d56291defd39b5cf0f91efeffac0a69dea90d1a
박찬영 <pcydeveloper@gmail.com>
2022-05-06 10:31:57 +0900

    chore: Nuxt3 추가

---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<style lang="less" scoped>
+
+</style>
+
+<template>
+  <div class="index">asd</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    console.log(333);  
+  },
+};
+</script>
