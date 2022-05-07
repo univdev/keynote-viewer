1a4455bef05b46d4accc41ff6b9af36041e44482
박찬영 <pcydeveloper@gmail.com>
2022-05-07 20:21:55 +0900

    chore: lang 파일 추가

---
+++ lang/en.ts
@@ -0,0 +1,21 @@
+export default {
+  global: {
+    title: 'Luxury',
+    locale: {
+      placeholder: '언어',
+    },
+  },
+  pages: {
+    home: {
+      title: '키노트 리스트',
+      form: {
+        keynote: {
+          placeholder: '키노트 파일을 업로드해주세요',
+        },
+        title: {
+          placeholder: '제목',
+        },
+      },
+    },
+  },
+};
---
+++ lang/ko.ts
@@ -0,0 +1,28 @@
+export default {
+  global: {
+    title: '고급진',
+    locale: {
+      placeholder: '언어',
+    },
+  },
+  pages: {
+    home: {
+      title: '키노트 리스트',
+      form: {
+        keynote: {
+          placeholder: '키노트 파일을 업로드해주세요',
+        },
+        title: {
+          placeholder: '제목',
+        },
+      },
+    },
+    error: {
+      messages: {
+        404: '페이지를 찾을 수 없습니다.',
+        500: '서버가 응답하지 않습니다.',
+        unknown: '알 수 없는 오류입니다.',
+      },
+    },
+  },
+};
